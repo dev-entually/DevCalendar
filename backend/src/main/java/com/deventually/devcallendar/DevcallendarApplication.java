bed5f8a03dc870de03607d203f8f603a4965e4d2
gurunelee <dlckdgk4858@gmail.com>
2022-10-15 22:24:57 +0900

    init: setup backend

---
+++ backend/src/main/java/com/deventually/devcallendar/DevcallendarApplication.java
@@ -0,0 +1,13 @@
+package com.deventually.devcallendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevcallendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevcallendarApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/deventually/devcallendar/DevcallendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deventually.devcallendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevcallendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
