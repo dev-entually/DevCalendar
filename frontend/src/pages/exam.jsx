acca80a3092e0351cb6965acf4f91269773d941f
okyungjin <okyungjin@gmail.com>
2022-10-30 23:53:28 +0900

    feat: file system based router 구성 (#18)

---
+++ frontend/src/pages/exam.jsx
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom';
+
+const Exam = () => (
+  <div>
+    <h1>Exam</h1>
+    <Link to="/">Home</Link>
+  </div>
+);
+
+export default Exam;
---
+++ frontend/src/pages/index.jsx
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom';
+
+const Home = () => (
+  <div>
+    <h1>Home</h1>
+    <Link to="/exam">Exam</Link>
+  </div>
+);
+
+export default Home;
