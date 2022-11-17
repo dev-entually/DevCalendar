43b525dc94f0cf568c5f660ffa3f65769d6c938c
okyungjin <okyungjin@gmail.com>
2022-11-17 14:41:06 +0900

    feat: 공통 컴포넌트 구현 CheckboxButtonInput (#12)

---
+++ frontend/src/components/input/CheckboxButtonInput.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { Button } from '../../components';
+import { Code, CheckableTagsInputProps } from '../../model/components/input';
+import styles from './CheckboxButtonInput.module.css';
+
+const removeAElementInArray = (elementToRemove: any, arr: any[]) => {
+  const targetIdx = arr.indexOf(elementToRemove);
+  if (targetIdx === -1) return arr;
+  const removed = [...arr];
+  removed.splice(targetIdx, 1);
+  return removed;
+};
+
+const CheckboxButtonInput: React.FC<CheckableTagsInputProps> = ({ options, selected, onChange }) => {
+  const onSelectedChange: React.MouseEventHandler<HTMLElement> = ({ currentTarget }) => {
+    const alreadySelected = selected.includes(currentTarget.id);
+    if (alreadySelected)
+      onChange(
+        removeAElementInArray(currentTarget.id, selected));
+    else
+      onChange([...selected, currentTarget.id]);
+  };
+
+  const selectedClassOrEmpty = (option: Code) => selected.includes(option.value) ? styles.selected : '';
+
+  return (
+    <div>
+      { options.map(option =>
+        <Button
+          className={`${styles.btn} ${selectedClassOrEmpty(option)}`}
+          key={option.value}
+          id={option.value}
+          label={option.label}
+          onClick={onSelectedChange} />
+      )}
+    </div>
+  );
+};
+
+export default CheckboxButtonInput;
--- frontend/src/pages/exam.tsx
+++ frontend/src/pages/exam.tsx
@@ -1,6 +1,16 @@
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from '../components';
 import '../assets/style.css';
+import CheckboxButtonInput from '../components/input/CheckboxButtonInput';
+import {CheckableTagsInputProps, Code} from '../model/components/input';
+
+const options: Code[] = [
+  { label: 'Code 1', value: 'code 1' },
+  { label: 'Code 2', value: 'code 2' },
+  { label: 'Code 3', value: 'code 3' },
+  { label: 'Code 4', value: 'code 4' },
+];
 
 const Exam = () => (
   <div>
@@ -24,7 +34,29 @@ const Exam = () => (
         <Button label="onClick" onClick={() => console.log('Button is clicked.')}/>
       </div>
     </div>
+
+    <section>
+      <h1>Input</h1>
+      <CheckboxButtonInputExam />
+    </section>
   </div>
 );
+
+const CheckboxButtonInputExam: React.FC = () => {
+  const [selected, setSelected] = useState<string[]>([])
+  return (
+    <>
+      <h2>CheckboxButtonInput</h2>
+      <pre>{selected}</pre>
+      <div className="wrap">
+        <CheckboxButtonInput
+          options={options}
+          selected={selected}
+          onChange={setSelected}
+          />
+      </div>
+    </>
+  );
+};
 
 export default Exam;
